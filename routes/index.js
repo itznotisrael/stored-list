1df0484a92fd7bae6c9d21129ae6b12b96a75a2b
Tiqon Lediard Jr. <lediardtiqon@gmail.com>
2025-12-02 14:51:46 -0500

    II

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+// REQUIRE YOUR MODEL HERE AT THE TOP
+const users = require('../models/users');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', list: [] });
+router.get('/', async function(req, res, next) {
+    try {
+      const data = await users.find();
+      res.render('index', { title: 'Express', users: data });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
 });
 
 module.exports = router;
